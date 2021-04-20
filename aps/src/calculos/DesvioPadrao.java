45b96388201bf20eade9a7724c36c32384f2a173
Matheus <64097029+Matheusje17@users.noreply.github.com>
2021-04-20 19:48:13 -0300

    Classes de Desvio padrão e variancia

---
+++ aps/src/calculos/DesvioPadrao.java
@@ -0,0 +1,17 @@
+package calculos;
+
+public class DesvioPadrao {
+	
+	private double desvioPadrao;
+	
+	public void calculaDesvio() {
+		Variancia var = new Variancia();
+		desvioPadrao = Math.sqrt(var.getVarianca());
+	}
+	
+	public double getDesvio() {
+		calculaDesvio();
+		return desvioPadrao;
+	}
+	
+}
---
+++ aps/src/calculos/Variancia.java
@@ -0,0 +1,32 @@
+package calculos;
+public class Variancia {
+	
+	private static final double testeMedia = 52;
+	private double[] dados = {158,143,66,38,24,15,11,8,5};
+	private double variancia;
+	
+	
+	public void calculaVarianca() {
+		
+		variancia = (Math.pow(dados[0] - testeMedia,2)) 
+				+ (Math.pow(dados[1] - testeMedia,2)) 
+				+ (Math.pow(dados[2] - testeMedia,2)) 
+				+ (Math.pow(dados[3] - testeMedia,2))
+				+ (Math.pow(dados[4] - testeMedia,2)) 
+				+ (Math.pow(dados[5] - testeMedia,2))
+				+ (Math.pow(dados[6] - testeMedia,2)) 
+				+ (Math.pow(dados[7] - testeMedia,2))
+				+ (Math.pow(dados[8] - testeMedia,2));
+		
+		this.variancia /=testeMedia;
+		
+		
+	}
+	
+	public double getVarianca() {
+		calculaVarianca();
+		
+		return variancia;
+	}
+	
+}
