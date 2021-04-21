3a8b4636bbb5961847f93c750b627bab1ce91d56
Gafanhoto do Bom <Gafanhoto do Bom@GAFANHOTO>
2021-04-20 22:18:12 -0300

    mudança main

--- aps/src/mainPack/MainTest.java
+++ aps/src/mainPack/MainTest.java
@@ -1,6 +1,7 @@
 package mainPack;
 
 import calculos.DesvioPadrao;
+import calculos.Mediana;
 import calculos.Variancia;
 
 public class MainTest {
@@ -8,10 +9,16 @@ public class MainTest {
 	public static void main(String[] args) {
 		Variancia var = new Variancia();
 		DesvioPadrao desvio = new DesvioPadrao();
+		Mediana meio = new Mediana();
 		
 		System.out.printf("Variância: %.2f",var.getVarianca());
 		System.out.println("");
 		System.out.printf("Desvio padrão é: %.2f",desvio.getDesvio());
+		System.out.println("");
+		System.out.println("Mediana é: " + meio.);
+		
+		
+
 
 	}
 
