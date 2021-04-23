d3ce4d26202cd5188b0ce80406497ed08d2fe4f9
Matheus <64097029+Matheusje17@users.noreply.github.com>
2021-04-22 21:21:32 -0300

    Mudança nas classes media e variancia

--- aps/src/calculos/Media.java
+++ aps/src/calculos/Media.java
@@ -3,13 +3,14 @@ package calculos;
 public class Media {
 	
 	private double[] dados = {158,143,66,38,24,15,11,8,5};
-	private double media;
+	private double media,soma;
 	
 	public double getMedia() {
 		for(int i = 0; i < dados.length;i++) {
-			media += dados[i];
+			soma += dados[i];
 		}
-		return media/=dados.length;	
+		media = soma / dados.length;
+		return media;
 	}
 	
 }
--- aps/src/calculos/Variancia.java
+++ aps/src/calculos/Variancia.java
@@ -4,23 +4,15 @@ public class Variancia {
 	Media media = new Media();
 	private double[] dados = {158,143,66,38,24,15,11,8,5};
 	private double variancia;
-	
+	private double soma;
 	
 	public void calculaVarianca() {
-		
-		variancia = (Math.pow(dados[0] - media.getMedia(),2)) 
-				+ (Math.pow(dados[1] - media.getMedia(),2)) 
-				+ (Math.pow(dados[2] - media.getMedia(),2)) 
-				+ (Math.pow(dados[3] - media.getMedia(),2))
-				+ (Math.pow(dados[4] - media.getMedia(),2)) 
-				+ (Math.pow(dados[5] - media.getMedia(),2))
-				+ (Math.pow(dados[6] - media.getMedia(),2))  
-				+ (Math.pow(dados[7] - media.getMedia(),2))
-				+ (Math.pow(dados[8] - media.getMedia(),2));
-		
-		this.variancia /=media.getMedia();
-		
-		
+		double m = media.getMedia();
+		for(int i = 0; i < dados.length;i++) {
+			soma = Math.pow(dados[i] - m,2);
+			variancia +=soma;
+		}
+		variancia /=m;
 	}
 	
 	public double getVarianca() {
