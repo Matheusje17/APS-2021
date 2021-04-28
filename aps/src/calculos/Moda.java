3d4120a16790a443b49d7232b46a41856edf40a0
Matheus <64097029+Matheusje17@users.noreply.github.com>
2021-04-28 19:14:09 -0300

    Teste retorno classe moda

--- aps/src/calculos/Moda.java
+++ aps/src/calculos/Moda.java
@@ -75,6 +75,7 @@ public class Moda {
 	public float getModa() {
 		calcularModa(numeros);
 		return this.getModa();
+		
 	}
 	
 	
--- aps/src/mainPack/MainTest.java
+++ aps/src/mainPack/MainTest.java
@@ -3,7 +3,7 @@ package mainPack;
 import calculos.DesvioPadrao;
 import calculos.Media;
 import calculos.Mediana;
-import calculos.Moda;
+//import calculos.Moda;
 import calculos.Variancia;
 
 public class MainTest {
@@ -12,23 +12,23 @@ public class MainTest {
 		Variancia var = new Variancia();
 		DesvioPadrao desvio = new DesvioPadrao();
 		Mediana meio = new Mediana();
-		Moda moda = new Moda();
+		//Moda moda = new Moda();
 		Media media = new Media();
 		
 		
 		System.out.println("Resultados Obtidos a partir do gráfico");
 		System.out.println("");
-		System.out.printf("Variância: %.2f",var.getVarianca());
+		System.out.printf("Variância: " + var.getVarianca());
 		System.out.println("");
 		System.out.println("");
-		System.out.printf("Desvio padrão é: %.2f",desvio.getDesvio());
+		System.out.printf("Desvio padrão é: "  +desvio.getDesvio());
 		System.out.println("");
 		System.out.println("");
 		System.out.println("Mediana é: " + meio.getResultado());
 		System.out.println("");
 		System.out.println("Media é: " + media.getMedia());
 		System.out.println("");
-		moda.getModa();
+		//moda.getModa();
 		
 	}
 
