3b9f57dce806e56d6a2ae83331ec2faa0b0e1b8e
Matheus <64097029+Matheusje17@users.noreply.github.com>
2021-04-29 21:07:17 -0300

    Add classe indicador e modificação grafico

---
+++ aps/src/calculos/Indicador.java
@@ -0,0 +1,42 @@
+package calculos;
+
+import java.util.List;
+
+import dados.CasosApontados;
+
+public class Indicador {
+	private List<CasosApontados> casos;
+	
+	public Indicador(List<CasosApontados> casos) {
+		this.casos=casos;
+	}
+
+	public String getMedia() {
+		Media m = new Media(this.casos);
+		return Double.toString(m.getMedia());
+	}
+
+
+	public String getCalcMediana() {
+
+		return null;
+	}
+
+	public String getCalcModa() {
+		return null;
+	}
+
+	public String getCalcDesvio() {
+		DesvioPadrao d = new DesvioPadrao(new Variancia(this.casos,Double.parseDouble(this.getMedia())));
+		return d.getDesvio();
+	}
+	
+
+	public String getCalcVariancia() {
+		Variancia v= new Variancia(this.casos,Double.parseDouble(this.getMedia())); 
+		return v.getVarianca();
+	}
+	
+	
+
+}
